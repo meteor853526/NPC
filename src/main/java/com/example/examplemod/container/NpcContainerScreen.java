88645e7da0531390d23e74aac0e50be47a8e646b
imlililili <78844463+imlililili@users.noreply.github.com>
2023-04-17 20:58:24 +0800

    png prob solved

--- src/main/java/com/example/examplemod/container/NpcContainerScreen.java
+++ src/main/java/com/example/examplemod/container/NpcContainerScreen.java
@@ -38,7 +38,7 @@ public class NpcContainerScreen extends ContainerScreen<NpcContainer> {
     protected void drawGuiContainerBackgroundLayer(MatrixStack pose, float partialTicks, int mouseX, int mouseY) {
         this.renderBackground(pose);
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.minecraft.getTextureManager().getTexture(NPC_CONTAINER_RESOURCE);
+        this.minecraft.getTextureManager().bindTexture(NPC_CONTAINER_RESOURCE);
         int i = (this.width - this.xSize) / 2;
         int j = (this.height - this.ySize) / 2;
         blit(pose, i, j, 0, 0, xSize, ySize, this.textureWidth, textureHeight);
