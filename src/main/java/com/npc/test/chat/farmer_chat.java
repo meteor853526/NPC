25cea83d4e2d055f999cdb36b88c01de33c8e982
platpus <48357121+beagoodgirl@users.noreply.github.com>
2023-05-09 14:01:31 +0800

    解決空白問題

--- src/main/java/com/npc/test/chat/farmer_chat.java
+++ src/main/java/com/npc/test/chat/farmer_chat.java
@@ -122,8 +122,6 @@ public class farmer_chat implements Runnable {
                                 + "If record show something which mean dont say hi again and DONT introduce yourself again and DONT say the setting! If record show nothing which mean this is the first time we met ."+ "(it is "+currentTime+" now.)"+"There is the current message: "
                                 + event.getMessage().replace("#",""));
 
-
-
                         System.out.println("Your reply need to below 50 words !!!!. You are a Non-Player Character(NPC) and your name is diedie and your duty is a farmer and sell some product to player in minecraft !!! .  Then is your character setting "
                                 + setting +"And this the record we talked before " + chatRecord
                                 + "If record show something which mean dont say hi again and DONT introduce yourself again and DONT say the setting! If record show nothing which mean this is the first time we met ."+ "(it is "+currentTime+" now.)"+"There is the current message: "
